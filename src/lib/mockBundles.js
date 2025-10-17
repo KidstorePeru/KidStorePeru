4e5a7c76a9e043b26057ae59511c4878ba588c3a
KidstorePeru <kidstoreperu.pe@gmail.com>
2025-10-17 05:05:43 -0500

    Update mockBundles.js

--- src/lib/mockBundles.js
+++ src/lib/mockBundles.js
@@ -3,7 +3,7 @@ export const specialBundles = [
     id: "HexedAndHauntedPack",
     nameEs: "Pack de Embrujos y hechicería",
     nameEn: "Hexed & Haunted Pack",
-    price: { PEN: 51.82, USD: 18.49, EUR: 18.49 }, //configurar precio segun moneda
+    price: { PEN: 45.90, USD: 14.90, EUR: 12.90 }, //configurar precio segun moneda
     image: "/images/1-icon.png", //Ubicacion public/images/
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
@@ -11,7 +11,7 @@ export const specialBundles = [
     id: "BeePositivePack",
     nameEs: "Pack de Abeja alegre",
     nameEn: "Bee Positive Pack",
-    price: { PEN: 13.20, USD: 4.49, EUR: 4.49 },
+    price: { PEN: 13.00, USD: 3.90, EUR: 3.40 },
     image: "/images/2-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
@@ -19,7 +19,7 @@ export const specialBundles = [
     id: "GildedElitesPack",
     nameEs: "Pack de Élite dorada",
     nameEn: "Gilded Elites Pack",
-    price: { PEN: 46.29, USD: 16.99, EUR: 16.99 },
+    price: { PEN: 42.90, USD: 13.80, EUR: 11.90 },
     image: "/images/3-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
@@ -27,7 +27,7 @@ export const specialBundles = [
     id: "SupernovaAcademyQuestPack",
     nameEs: "Pack de misiones de Academia Supernova",
     nameEn: "Supernova Academy Quest Pack",
-    price: { PEN: 28.32, USD: 8.99, EUR: 8.99 },
+    price: { PEN: 24.90, USD: 7.99, EUR: 6.99 },
     image: "/images/4-icon.png",
     expiresAt: "2025-10-17T19:00:00Z", // Configurar tiempo en la tienda
   },
@@ -35,7 +35,7 @@ export const specialBundles = [
     id: "SkullSquadPack",
     nameEs: "Pack de Escuadrón calavera",
     nameEn: "Skull Squad Pack",
-    price: { PEN: 63.92, USD: 22.99, EUR: 22.99 },
+    price: { PEN: 56.90, USD: 18.20, EUR: 15.90 },
     image: "/images/5-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
@@ -43,7 +43,7 @@ export const specialBundles = [
     id: "ShadowsRisingPack",
     nameEs: "Pack Alzamiento sombrío",
     nameEn: "Shadows Rising Pack",
-    price: { PEN: 51.82, USD: 18.49, EUR: 18.49 },
+    price: { PEN: 45.90, USD: 14.90, EUR: 12.90 },
     image: "/images/6-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
@@ -51,7 +51,7 @@ export const specialBundles = [
     id: "PixelleStarterPack",
     nameEs: "Pack de inicio de Pixelia",
     nameEn: "Pixelle Starter Pack",
-    price: { PEN: 14.99, USD: 4.99, EUR: 4.99 },
+    price: { PEN: 13.90, USD: 4.30, EUR: 3.90 },
     image: "/images/7-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
@@ -59,7 +59,7 @@ export const specialBundles = [
   id: "FortniteCrew",
   nameEs: "Club de Fortnite",
   nameEn: "Fortnite Crew",
-  price: { PEN: 20.00, USD: 6.40, EUR: 5.70 },
+  price: { PEN: 20.00, USD: 6.40, EUR: 5.50 },
   image: "/images/8-icon.png", // La imagen que quieras usar para el club
   expiresAt: "2025-10-31T19:00:00Z", 
 }
