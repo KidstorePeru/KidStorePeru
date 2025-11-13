f0597ef7e9de66a0c78b02e5d0a43a02185e39b7
KidstorePeru <kidstoreperu.pe@gmail.com>
2025-11-12 20:01:07 -0500

    Update mockBundles.js

--- src/lib/mockBundles.js
+++ src/lib/mockBundles.js
@@ -1,21 +1,5 @@
 export const specialBundles = [
   {
-    id: "HexedAndHauntedPack",
-    nameEs: "Pack de Embrujos y hechicería",
-    nameEn: "Hexed & Haunted Pack",
-    price: { PEN: 45.90, USD: 14.20, EUR: 12.10 }, //configurar precio segun moneda
-    image: "/images/1-icon.png", //Ubicacion public/images/
-    expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
-  },
-  {
-    id: "BeePositivePack",
-    nameEs: "Pack de Abeja alegre",
-    nameEn: "Bee Positive Pack",
-    price: { PEN: 13.00, USD: 3.99, EUR: 3.42 },
-    image: "/images/2-icon.png",
-    expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
-  },
-  {
     id: "GildedElitesPack",
     nameEs: "Pack de Élite dorada",
     nameEn: "Gilded Elites Pack",
@@ -24,27 +8,19 @@ export const specialBundles = [
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
   {
-    id: "SupernovaAcademyQuestPack",
-    nameEs: "Pack de misiones de Academia Supernova",
-    nameEn: "Supernova Academy Quest Pack",
-    price: { PEN: 24.90, USD: 7.99, EUR: 6.99 },
-    image: "/images/4-icon.png",
-    expiresAt: "2025-10-17T19:00:00Z", // Configurar tiempo en la tienda
-  },
-  {
-    id: "SkullSquadPack",
-    nameEs: "Pack de Escuadrón calavera",
-    nameEn: "Skull Squad Pack",
-    price: { PEN: 56.90, USD: 17.60, EUR: 14.99 },
-    image: "/images/5-icon.png",
+    id: "SkateParkRoyaltyPack",
+    nameEs: "Pack Amo del Skatepark",
+    nameEn: "Skate Park Royalty Pack",
+    price: { 45.90, USD: 14.20, EUR: 12.10 },
+    image: "/images/10-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
   {
-    id: "ShadowsRisingPack",
-    nameEs: "Pack Alzamiento sombrío",
-    nameEn: "Shadows Rising Pack",
-    price: { PEN: 45.90, USD: 14.20, EUR: 12.10 },
-    image: "/images/6-icon.png",
+    id: "TheDiamondDivaPack",
+    nameEs: "Pack Diva de Diamantes",
+    nameEn: "The Diamond Diva Pack",
+    price: { PEN: 13.00, USD: 3.99, EUR: 3.42 },
+    image: "/images/11-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
   {
@@ -59,7 +35,7 @@ export const specialBundles = [
   id: "FortniteCrew",
   nameEs: "Club de Fortnite",
   nameEn: "Fortnite Crew",
-  price: { PEN: 20.00, USD: 6.20, EUR: 5.30 },
+  price: { PEN: 22.00, USD: 6.80, EUR: 5.80 },
   image: "/images/8-icon.png", // La imagen que quieras usar para el club
   expiresAt: "2025-10-31T19:00:00Z", 
 }
