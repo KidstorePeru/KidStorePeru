c3010996fc817bdc0db2e2a16f351d8dc1146401
KidstorePeru <kidstoreperu.pe@gmail.com>
2026-01-19 16:51:48 -0500

    Update mockBundles.js

--- src/lib/mockBundles.js
+++ src/lib/mockBundles.js
@@ -1,26 +1,10 @@
 export const specialBundles = [
   {
-    id: "GuardiansoftheGalaxyPack",
-    nameEs: "Pack Guardianes de la Galaxia",
-    nameEn: "Guardians of the Galaxy Pack",
-    price: { PEN: 58.99, USD: 18.99, EUR: 15.99 },
-    image: "/images/17-icon.png",
-    expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
-  },
-  {
     id: "FullClipPack",
     nameEs: "Pack Clip al Completo",
     nameEn: "FullClipPack",
     price: { PEN: 41.99, USD: 12.99, EUR: 10.99 },
     image: "/images/12-icon.png",
-    expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
-  },
-  {
-    id: "PolarLegendsPack",
-    nameEs: "Pack Leyendas Polares",
-    nameEn: "Polar Legends Pack",
-    price: { PEN: 26.90, USD: 8.99, EUR: 7.30 },
-    image: "/images/14-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
   {
@@ -32,19 +16,11 @@ export const specialBundles = [
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
   {
-    id: "AgencyRenegadesPack",
-    nameEs: "Pack Renegados de la Agencia",
-    nameEn: "Agency Renegades Pack",
-    price: { PEN: 24.90, USD: 7.90, EUR: 6.90 },
-    image: "/images/15-icon.png",
-    expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
-  },
-  {
-    id: "FrozenLegendsPack",
-    nameEs: "Pack Leyendas Congeladas",
-    nameEn: "Frozen Legends Pack",
-    price: { PEN: 19.90, USD: 6.50, EUR: 5.50 },
-    image: "/images/13-icon.png",
+    id: "BassassinQuestPack",
+    nameEs: "Pack de Misiones de Siluro Asueldo",
+    nameEn: "Bassassin Quest Pack",
+    price: { PEN: 20.90, USD: 6.99, EUR: 5.99 },
+    image: "/images/19-icon.png",
     expiresAt: "2025-11-03T19:00:00Z", // Configurar tiempo en la tienda
   },
   {
